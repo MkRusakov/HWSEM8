f2244f1317ec41d74bf994504d88f626b3a03e51
Mikhail <i@MkRusakov.ru>
2022-09-02 19:27:57 +0300

    task56

--- Task56/Program.cs
+++ Task56/Program.cs
@@ -22,10 +22,38 @@ int[,] CreateMatrixArray (int row, int column, int min, int max)
     }
     return array;
 }
-void SearchMinimalSum (int[,] array)
+int[] SearchRowSum (int[,] array)
 {
-    
+    int sum = 0;
+    int[] arrayOfSum = new int[array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        arrayOfSum[i] = sum;
+        sum = 0;
+        Console.WriteLine($" Для удобства сумма каждой строки {i+1}: {arrayOfSum[i]} ");
+    }
+    return arrayOfSum;
+}
+void SearchIndexMinimalSum (int[] arrayOfSum)
+{
+    int min = arrayOfSum[0];
+    int index = 0;
+    for (int i = 0; i < arrayOfSum.Length; i++)
+    {
+        if (arrayOfSum[i] < min)
+        {
+            index = i;
+            min = arrayOfSum[i];
+        }
+    }
+    Console.WriteLine($"Строка с наименьшей суммой элементов - {index+1} строка");
 }
 int[,] myArray = CreateMatrixArray(3,3,1,8);
 PrintArray(myArray);
-SearchMinimalSum(myArray);
+Console.WriteLine();
+int[] arrayOfSum = SearchRowSum(myArray);
+SearchIndexMinimalSum(arrayOfSum);
